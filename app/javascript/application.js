aa67fab0567e725121a8da2d21987f3717e50f07
tomohiko.kuzuba <tomo.k9090@gmail.com>
2025-01-03 14:18:24 +0900

    doc: コメントアウトを削除

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
 // stimulusが読み込めなかったため暫定対応
 const importMap = JSON.parse(document.querySelector('script[type="importmap"]').textContent);
 
 import("@hotwired/stimulus").then(module => {
   const app = module.Application.start();
   window.Stimulus = app;
-  
   import(importMap.imports["controllers/star_rating_controller"]).then(controller => {
     app.register("star-rating", controller.default);
   });
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,12 +1,3 @@
-# pin "application"
-# pin "@hotwired/turbo-rails", to: "turbo.min.js"
-# pin "@hotwired/stimulus", to: "stimulus.min.js"
-# pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-# pin_all_from "app/javascript/controllers", under: "controllers"
-# # 他の必要なパッケージ
-# pin "chartkick", to: "chartkick.js"
-# pin "Chart.bundle", to: "Chart.bundle.js"
-
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
