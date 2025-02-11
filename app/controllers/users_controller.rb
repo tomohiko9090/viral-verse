ec31aa814a9a7fd82625dca580f3152d0be0b85d
tomohiko.kuzuba <tomo.k9090@gmail.com>
2025-02-11 20:47:27 +0900

    fix: 多店舗登録のユーザーの作成の処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,16 +9,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    ActiveRecord::Base.transaction do
-      if @user.save
-        @shop.shop_users.create!(user: @user)
-        flash[:success] = 'ユーザーを追加しました'
-        redirect_to shop_path(@shop)
-      else
-        flash[:danger] = 'ユーザーの追加に失敗しました'
-        render :new
-      end
+    if user_params[:password].blank? && user_params[:name].blank? && user_params[:email].present?
+      # 他店舗登録の場合（上の条件の時だけ特別に他店舗登録する）
+      create_multi_shop_owner
+    else
+      # 新規登録の場合
+      create_new_owner
     end
   end
 
@@ -36,6 +32,42 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def create_multi_shop_owner
+    user = User.find_by(email: user_params[:email])
+
+    if user.present?
+      ActiveRecord::Base.transaction do
+        unless @shop.users.exists?(id: user.id)
+          @shop.shop_users.create!(user: user)
+          flash[:success] = '既存ユーザーを店舗に追加しました'
+          redirect_to shop_path(@shop)
+        else
+          flash[:danger] = 'このユーザーは既にこの店舗に登録されています'
+          @user = @shop.users.build(user_params)
+          render :new
+        end
+      end
+    else
+      flash[:danger] = 'メールアドレスに一致するユーザーが見つかりません。新規登録の場合は名前とパスワードを入力してください'
+      @user = @shop.users.build(user_params)
+      render :new
+    end
+  end
+
+  def create_new_owner
+    @user = User.new(user_params)
+    ActiveRecord::Base.transaction do
+      if @user.save
+        @shop.shop_users.create!(user: @user)
+        flash[:success] = 'ユーザーを追加しました'
+        redirect_to shop_path(@shop)
+      else
+        flash[:danger] = 'ユーザーの追加に失敗しました'
+        render :new
+      end
+    end
+  end
 
   def set_shop
     @shop = Shop.find(params[:shop_id])
