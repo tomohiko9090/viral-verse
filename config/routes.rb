49385c78d23dd2f118f6c8c22912d34fd8e3f087
tomohiko.kuzuba <tomo.k9090@gmail.com>
2025-02-23 08:33:17 +0900

    feat: 英語で回答画面を表示できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -12,19 +12,27 @@ Rails.application.routes.draw do
     resources :users, except: [:index, :show]
 
     # レビュー関連
-    resources :reviews, only: [:index, :new, :create] do
+    resources :reviews, only: [:index, :create] do
+      collection do
+        get 'new(/:locale)', to: 'reviews#new', as: :localized_new
+      end
       member do
-        get :notice
-        get :survey1
-        get :survey2
-        post :submit_survey1
-        post :submit_survey2
+        scope '(:locale)', locale: /en|ja/ do
+          # ロケール付きのURLが生成できないため、as: :localized_notice を定義している
+          # 例) localized_notice_shop_review_path(@shop, @review, locale: params[:locale])
+          get 'notice', to: 'reviews#notice', as: :localized_notice
+          get 'survey1', to: 'reviews#survey1', as: :localized_survey1
+          get 'survey2', to: 'reviews#survey2', as: :localized_survey2
+          post 'submit_survey1', to: 'reviews#submit_survey1', as: :localized_submit_survey1
+          post 'submit_survey2', to: 'reviews#submit_survey2', as: :localized_submit_survey2
+        end
       end
     end
   end
 
   root "shops#index"
   match '*path', to: 'application#not_found', via: :all
+
   # MEMO: 以下とGemfileのerd_mapをコメントアウトし、bundle exec rails erd_mapを実行する
   # mount ErdMap::Engine => "erd_map"
 end
