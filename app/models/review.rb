40409f75c58e2c9f3357b94a3742e6a0532d9dc8
tomohiko.kuzuba <tomo.k9090@gmail.com>
2025-04-06 17:48:15 +0900

    feat: レビュアーがログインしてるなら、無効にする

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :shop
+  belongs_to :user, optional: true  # ログインしてないならnullが入るようにする
 
   validates :score, presence: true, inclusion: { in: 1..5, message: "は1から5の間で入力してください" }
   validates :comments, presence: true, length: { maximum: 1000 }
